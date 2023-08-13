4d0cfee090c002a921ada8bb6b1fe78329148eda
Zachary Honey <zhoney466@gmail.com>
2023-08-12 21:36:01 -0700

    Saving to not lose progress

--- src/components/FavoriteMovieList.js
+++ src/components/FavoriteMovieList.js
@@ -2,24 +2,31 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
+import { connect } from 'react-redux';
+
 
 const FavoriteMovieList = (props) => {
-    const favorites = [];
-    
+    const { favorites } = props;
+
     return (<div className="col-xs savedContainer">
         <h5>Favorite Movies</h5>
         {
-            favorites.map(movie=>{
+            favorites.map(movie => {
                 return <div key={movie.id}>
                     <Link className="btn btn-light savedButton" to={`/movies/${movie.id}`}>
                         {movie.title}
                         <span><span class="material-icons">remove_circle</span></span>
-                    </Link> 
+                    </Link>
                 </div>
             })
         }
     </div>);
 }
 
+const mapStateToProps = (state) => {
+    return {
+        favorites: state.favoritesReducer.favorites
+    }
+}
 
-export default FavoriteMovieList;
+export default connect(mapStateToProps)(FavoriteMovieList);
